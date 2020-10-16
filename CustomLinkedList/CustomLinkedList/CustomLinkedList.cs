9e23ec9b3dd0c7e26e9d588ac05db01746ed71df
Denis Nalivkin <proshivakdocument@yandex.ru>
2020-10-16 13:26:42 +0300

    I have started work on the linked list.

---
+++ CustomLinkedList/CustomLinkedList/CustomLinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+    class CustomLinkedList <T>
+    {
+        Node<T> head {get; set;}
+        Node<T> tail {get; set;}
+        public int Count {get; set;}
+
+        public CustomLinkedList()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ CustomLinkedList/CustomLinkedList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+  public class Node <T>
+    {
+        T value { get; set; }
+        Node<T> previousNode { get; set; }
+        Node<T> nextNode { get; set; }
+
+        public Node()
+        {
+        
+        } 
+
+    }
+}
